f45e48c9089af00501c6ac770de2376b47842cc3
Brave Leuterio <you@example.com>
2020-10-01 17:53:57 +0800

    added foundation files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+//f2be2c6245a797ea9384a28cff7ead94
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+
+
+      </body>
     </div>
   );
+
 }
 
 export default App;
